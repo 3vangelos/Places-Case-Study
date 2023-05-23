69ac91a0aaca53a8ac50aedb428a37e664fe724c
3vangelos <evangelos@posteo.eu>
2023-05-23 21:57:28 +0200

    Create Places Framework

---
+++ Places/Places/Places.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Places.
+FOUNDATION_EXPORT double PlacesVersionNumber;
+
+//! Project version string for Places.
+FOUNDATION_EXPORT const unsigned char PlacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Places/PublicHeader.h>
+
+
