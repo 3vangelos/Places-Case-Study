b776bdf1ac183bc28174a335bcdb364804c92670
3vangelos <evangelos@posteo.eu>
2023-05-23 23:04:16 +0200

    Refactors Duplication

--- Places/PlacesTests/GooglePlacesLoaderTests.swift
+++ Places/PlacesTests/GooglePlacesLoaderTests.swift
@@ -18,32 +18,38 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        requestedURL = url
-    }
-    
-    var requestedURL: URL?
-}
-
 class GooglePlacesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = GooglePlacesLoader(url: url, client: client)
-        
+        let (_, client) = makeSUT()
+
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = GooglePlacesLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    // MARK - Helpers
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: GooglePlacesLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = GooglePlacesLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+
 }
