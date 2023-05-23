d11dd0d9c7a46042a48eb752b95101d1cbcffddf
3vangelos <evangelos@posteo.eu>
2023-05-23 23:13:07 +0200

    Moves Loader component to production

--- Places/PlacesTests/GooglePlacesLoaderTests.swift
+++ Places/PlacesTests/GooglePlacesLoaderTests.swift
@@ -1,22 +1,6 @@
 import XCTest
+import Places
 
-class GooglePlacesLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
 
 class GooglePlacesLoaderTests: XCTestCase {
 
