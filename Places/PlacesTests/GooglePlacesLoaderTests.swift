aafa643abff23d0f3d777ae794e7b8545dec2fe4
3vangelos <evangelos@posteo.eu>
2023-05-23 22:35:43 +0200

    Requests Data from URL on load

--- Places/PlacesTests/GooglePlacesLoaderTests.swift
+++ Places/PlacesTests/GooglePlacesLoaderTests.swift
@@ -1,20 +1,35 @@
 import XCTest
 
 class GooglePlacesLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class GooglePlacesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = GooglePlacesLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = GooglePlacesLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
