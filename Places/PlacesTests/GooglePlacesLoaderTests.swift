4529e6c681575af37a664f6cb534fe54bd6ea535
3vangelos <evangelos@posteo.eu>
2023-05-23 23:33:04 +0200

    Delivers Connectivity Error on Client Error

--- Places/PlacesTests/GooglePlacesLoaderTests.swift
+++ Places/PlacesTests/GooglePlacesLoaderTests.swift
@@ -29,12 +29,26 @@ class GooglePlacesLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: GooglePlacesLoader.Error?
+        sut.load { error in capturedError = error }
+    
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK - Helpers
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = []
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
