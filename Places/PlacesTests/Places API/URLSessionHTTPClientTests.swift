8fb35817642f2e8de437c71366bec11daa207a53
3vangelos <evangelos@posteo.eu>
2023-05-24 21:21:01 +0200

    Moves SUT creation to separate method to protect against breaking changes

--- Places/PlacesTests/Places API/URLSessionHTTPClientTests.swift
+++ Places/PlacesTests/Places API/URLSessionHTTPClientTests.swift
@@ -40,7 +40,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -50,10 +50,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "Any Error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -66,6 +64,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
     }
     
     private class URLProtocolStub: URLProtocol {
