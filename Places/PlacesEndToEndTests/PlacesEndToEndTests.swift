d3046c76a7a0f3489ebac92db96b2f950dbd1788
3vangelos <evangelos@posteo.eu>
2023-05-25 09:45:22 +0200

    Added Memory Leak Tracking to End to End Tests

--- Places/PlacesEndToEndTests/PlacesEndToEndTests.swift
+++ Places/PlacesEndToEndTests/PlacesEndToEndTests.swift
@@ -17,14 +17,15 @@ switch getPlacesResult() {
     }
     
     // MARK - Helpers
-    
-    func getPlacesResult() -> LoadPlacesResult? {
+
+    func getPlacesResult(file: StaticString = #filePath, line: UInt = #line) -> LoadPlacesResult? {
         let urlString = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee&location=\(13.4050)%2C-\(52.52)&radius=500&key="
         let apiKey = "ENTER YOUR API KEY HERE"
         let url = URL(string: urlString + apiKey)!
         let client = URLSessionHTTPClient()
         let loader = GooglePlacesLoader(url: url, client: client)
-        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Waiting for Places to Load...")
         
         var receivedResult: LoadPlacesResult?
