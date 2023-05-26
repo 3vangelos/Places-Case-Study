fbf0c355d81f06a396f27e322a4168323e52d67b
3vangelos <evangelos@posteo.eu>
2023-05-26 11:11:14 +0200

    Extract sut creation to a factory method to protect tests against breaking changes

--- Places/PlacesTests/Places Cache/LoadPlacesFromCacheTests.swift
+++ Places/PlacesTests/Places Cache/LoadPlacesFromCacheTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Places
 
 class LocalPlacesLoader {
     let store: PlacesStore
@@ -6,18 +7,51 @@ class LocalPlacesLoader {
     init(store: PlacesStore) {
         self.store = store
     }
+    
+    func save(_ places: [Place]) {
+        store.deleteCachedPlaces()
+    }
 }
 
 class PlacesStore {
     var deleteCachedPlacesCount = 0
+    
+    func deleteCachedPlaces() {
+        deleteCachedPlacesCount += 1
+    }
 }
 
 class LoadPlacesFromCacheTests: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = PlacesStore()
-        _ = LocalPlacesLoader(store: store)
-        
+        let (_, store) = makeSUT()
+
         XCTAssertEqual(store.deleteCachedPlacesCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let (sut, store) = makeSUT()
+        let places = [uniquePlace(), uniquePlace()]
+        
+        sut.save(places)
+        
+        XCTAssertEqual(store.deleteCachedPlacesCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: LocalPlacesLoader, store: PlacesStore) {
+        let store = PlacesStore()
+        let sut = LocalPlacesLoader(store: store)
+        return (sut, store)
+    }
+    
+    private func uniquePlace() -> Place {
+        Place(id: UUID().uuidString,
+              name: "Any NAme",
+              category: nil,
+              imageUrl: nil,
+              location: Location(latitude: 1,
+                                 longitude: 1))
     }
 }
