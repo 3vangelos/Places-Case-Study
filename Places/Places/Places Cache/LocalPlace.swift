bd67d33bcff4ba9769fa0e35034447a069a3d8a3
3vangelos <evangelos@posteo.eu>
2023-05-27 23:27:03 +0200

    Moves Local Place to its own file

---
+++ Places/Places/Places Cache/LocalPlace.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct LocalPlace: Equatable {
+    public let id: String
+    public let name: String
+    public let category: String?
+    public let imageUrl: URL?
+    public let location: Location
+    
+    public init(id: String, name: String, category: String?, imageUrl: URL?, location: Location) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.imageUrl = imageUrl
+        self.location = location
+    }
+}
--- Places/Places/Places Cache/PlacesStore.swift
+++ Places/Places/Places Cache/PlacesStore.swift
@@ -7,19 +7,3 @@ public protocol PlacesStore {
     func deleteCachedPlaces(completion: @escaping DeletionCompletion)
     func insert(_ places: [LocalPlace], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-public struct LocalPlace: Equatable {
-    public let id: String
-    public let name: String
-    public let category: String?
-    public let imageUrl: URL?
-    public let location: Location
-    
-    public init(id: String, name: String, category: String?, imageUrl: URL?, location: Location) {
-        self.id = id
-        self.name = name
-        self.category = category
-        self.imageUrl = imageUrl
-        self.location = location
-    }
-}
