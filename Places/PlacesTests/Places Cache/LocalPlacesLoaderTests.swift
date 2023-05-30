eee4c97f2f51f0affb0a2fe1dbfbca3146d911ad
3vangelos <evangelos@posteo.eu>
2023-05-30 08:41:18 +0200

    Moved duplicated PlacesStoreSpy to separate Helper File

--- Places/PlacesTests/Places Cache/LocalPlacesLoaderTests.swift
+++ Places/PlacesTests/Places Cache/LocalPlacesLoaderTests.swift
@@ -122,44 +122,6 @@ class LocalPlacesLoaderTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, expectedError, file: file, line: line)
     }
     
-    private class PlacesStoreSpy: PlacesStore {
-        enum ReceivedMessage: Equatable {
-            case deleteCachedPlaces
-            case insert([LocalPlace], Date)
-        }
-        
-        private var insertionCompletions = [InsertionCompletion]()
-        private var deletionCompletions = [DeletionCompletion]()
-        private(set) var receivedMessages = [ReceivedMessage]()
-        
-        
-        func insert(_ places: [LocalPlace], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insert(places, timestamp))
-        }
-        
-        func deleteCachedPlaces(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCachedPlaces)
-        }
-        
-        func completeDeletion(with error: Error?, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func completeInsertion(with error: Error?, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
-    
     private func uniquePlace() -> Place {
         Place(id: UUID().uuidString,
               name: "Any NAme",
