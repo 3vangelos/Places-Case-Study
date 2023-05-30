b46a93478bc267043f41ca3ac41eb7f77fc38b82
3vangelos <evangelos@posteo.eu>
2023-05-30 17:30:41 +0200

    Local Place Loader does not message the store upon creation in order to validate cache

---
+++ Places/PlacesTests/Places Cache/ValidatePlacesCacheTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+import Places
+
+class ValidatePlacesCacheTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalPlacesLoader, store: PlacesStoreSpy) {
+        let store = PlacesStoreSpy()
+        let sut = LocalPlacesLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+}
