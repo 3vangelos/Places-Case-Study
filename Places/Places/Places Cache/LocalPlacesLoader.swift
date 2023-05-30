be00d415b4ad416eebb257bd97500466d04bdac1
3vangelos <evangelos@posteo.eu>
2023-05-30 18:46:00 +0200

    Makes PlacesCache Policy static as it needs no identit/state

--- Places/Places/Places Cache/LocalPlacesLoader.swift
+++ Places/Places/Places Cache/LocalPlacesLoader.swift
@@ -1,13 +1,15 @@
 import Foundation
 
-public final class CachePlacesPolicy {
-    private lazy var calendar = Calendar(identifier: .gregorian)
+private final class CachePlacesPolicy {
+    private init() {}
     
-    private var maxCacheAgeInDays: Int {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
         return 1
     }
     
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
@@ -19,12 +21,10 @@ public final class CachePlacesPolicy {
 public final class LocalPlacesLoader: PlacesLoader {
     private let store: PlacesStore
     private let currentDate: () -> Date
-    private let cachePolicy: CachePlacesPolicy
     
     public init(store: PlacesStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
-        self.cachePolicy = CachePlacesPolicy()
     }
 }
  
@@ -39,7 +39,7 @@ extension LocalPlacesLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .found(places, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(places, timestamp) where CachePlacesPolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(places.toModels()))
                 
             case .found, .empty:
@@ -58,7 +58,7 @@ extension LocalPlacesLoader {
             case .failure:
                 store.deleteCachedPlaces(completion: { _ in })
                 
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !CachePlacesPolicy.validate(timestamp, against: self.currentDate()):
                 store.deleteCachedPlaces(completion: { _ in })
                 
             case .empty, .found:
