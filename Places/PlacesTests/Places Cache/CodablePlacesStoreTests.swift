012a430c51865ea978a0c619aae06c2b3fd5a266
3vangelos <evangelos@posteo.eu>
2023-05-31 07:30:39 +0200

    Adds Test to check if Retrieval has no Side Effect

--- Places/PlacesTests/Places Cache/CodablePlacesStoreTests.swift
+++ Places/PlacesTests/Places Cache/CodablePlacesStoreTests.swift
@@ -27,6 +27,27 @@ class CodablePlacesStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for Retrieval")
+        
+        sut.retrieve { resultFirst in
+            sut.retrieve { resultSecond in
+                switch (resultFirst, resultSecond) {
+                case (.empty, .empty):
+                    break
+                default:
+                    XCTFail("Expected empty result, got \(resultFirst) and \(resultSecond) instead")
+                }
+                
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    
     // Mark: Helpers
     
     func makeSUT() -> CodablePlacesStore {
