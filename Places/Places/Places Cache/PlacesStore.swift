ec8b5938142fbd149cc4607aa9a244f1e0c55ad9
3vangelos <evangelos@posteo.eu>
2023-05-31 18:17:19 +0200

    Add comments to indicate to clients that they are responsible in which thread completions should run

--- Places/Places/Places Cache/PlacesStore.swift
+++ Places/Places/Places Cache/PlacesStore.swift
@@ -11,7 +11,15 @@ public protocol PlacesStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrievedCachedPlacesResult) -> Void
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func deleteCachedPlaces(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func insert(_ places: [LocalPlace], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func retrieve(completion: @escaping RetrievalCompletion)
 }
